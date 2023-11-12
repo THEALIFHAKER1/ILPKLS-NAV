5276df4dd8f0a995ba02ae21f9d306e862144a3e
THEALIFHAKER1 <alifhaker1@gmail.com>
2023-11-13 07:20:18 +0800

    Fix connection error in database.php

--- Env/database.php
+++ Env/database.php
@@ -14,7 +14,7 @@ try {
 }
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-// if (!$con) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
