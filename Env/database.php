938461a41aea51b49d042011cf0069ad5272826f
THEALIFHAKER1 <alifhaker1@gmail.com>
2023-11-13 07:20:02 +0800

    Refactor database connection code to use PDO and add error handling

--- Env/database.php
+++ Env/database.php
@@ -1,12 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "ilpkls-nav";
-$con = mysqli_connect($host, $user, $password, $database);
-if (mysqli_connect_error()) {
-    echo "FAIL";
-    exit();
-} else {
+require_once __DIR__ . '/../Env/env.php';
+
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+    ]);
+} catch (PDOException $e) {
+    echo '<h1 class="z-50 relative text-3xl font-bold underline text-red-500">' . $e->getMessage() . '</h1>';
+    exit;
 }
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+// if (!$con) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
 ?>
