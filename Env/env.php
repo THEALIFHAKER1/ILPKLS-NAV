3135f09e803d13bad6c4e6053ef3c8e18c1d5fb5
THEALIFHAKER1 <alifhaker1@gmail.com>
2023-11-15 05:40:53 +0800

    Remove unnecessary code and update database password in env.php

--- Env/env.php
+++ Env/env.php
@@ -4,12 +4,3 @@ $db_host = 'localhost';
 $db_user = 'root';
 $db_name = 'ilpkls-nav';
 $db_pass = '';
-// url
-// $website_url = 'http';
-// isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? $website_url .= 's' : '';
-// $website_url .= '://';
-// $website_url .= isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
-// $website_url .= '/';
-// $website_url = 'http://localhost/ILPKLS-NAV/';
-// echo $website_url;
-$website_url = 'http://localhost/ILPKLS-NAV/';
